7cf3c974a7be26d370852ffbb90b290dfb71a667
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-17 23:32:06 -0300

    parte 9

--- work004_filesytem_commandlineargs/003.js
+++ work004_filesytem_commandlineargs/003.js
@@ -2,10 +2,20 @@
 // node 003.js adicionar --title="coisas a comprar"
 // node 003.js help
 // node 003.js --version
+//outro1: node 003.js --help
+//outro2: node 003.js adicionar2
 
 const yargs = require("yargs")
 
 console.log(process.argv)
+
+// criar add command, usar o (outro1)
+yargs.command({
+    command: 'adicionar2',
+    describe: 'adicionar uma nova nota',
+    handler: function () { console.log('Adicionando uma nova nota')}
+})
+
 
 // customizar yargs
 yargs.version('1.1.0')
