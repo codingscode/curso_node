3e709af3799a446c5e522768e2538a8e9a7a3db6
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-17 23:08:35 -0300

    parte 8

--- work004_filesytem_commandlineargs/003.js
+++ work004_filesytem_commandlineargs/003.js
@@ -1,13 +1,11 @@
-// node 002.js
-// node 002.js adicionar --title="coisas a comprar"
-// node 002.js help
-// node 002.js --version
+// node 003.js
+// node 003.js adicionar --title="coisas a comprar"
+// node 003.js help
+// node 003.js --version
 
 const yargs = require("yargs")
 
-
 console.log(process.argv)
-console.log(yargs.argv)
 
 // customizar yargs
 yargs.version('1.1.0')
