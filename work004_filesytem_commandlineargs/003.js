c52b62109101bcce605b0f8eaa593d6631be2d35
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-17 23:39:30 -0300

    parte 10

--- work004_filesytem_commandlineargs/003.js
+++ work004_filesytem_commandlineargs/003.js
@@ -4,16 +4,25 @@
 // node 003.js --version
 //outro1: node 003.js --help
 //outro2: node 003.js adicionar2
+//outro3: node 003.js --help
+//outro4: node 003.js remover2
 
 const yargs = require("yargs")
 
 console.log(process.argv)
 
-// criar add command, usar o (outro1)
+// criar add command, usar o (outro1, 2)
 yargs.command({
     command: 'adicionar2',
     describe: 'adicionar uma nova nota',
     handler: function () { console.log('Adicionando uma nova nota')}
+})
+
+// criar remove command, usar o (outro3, 4)
+yargs.command({
+    command: 'remover2',
+    describe: 'remover uma nota',
+    gerenciador: function () { console.log('Removendo uma nota')}
 })
 
 
