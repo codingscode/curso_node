d85fd867dd9e7215f956f4c7da90a3da03c89d08
Heronnes Pereira <heronnestg@hotmail.com>
2020-05-17 23:01:20 -0300

    parte 7

---
+++ work004_filesytem_commandlineargs/002.js
@@ -0,0 +1,16 @@
+// node 002.js
+// node 002.js adicionar --title="coisas a comprar"
+// node 002.js help
+// node 002.js --version
+
+const yargs = require("yargs")
+
+
+console.log(process.argv)
+console.log(yargs.argv)
+
+
+
+
+
+
