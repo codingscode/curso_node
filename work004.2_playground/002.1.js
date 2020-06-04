f5ab7142a176159c8bed91e944691e6bd4cfca71
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-04 17:05:03 -0300

    parte 23

--- work004.2_playground/002.1.js
+++ work004.2_playground/002.1.js
@@ -1,8 +1,14 @@
 const arquivo_sincronizado = require('fs')
 
+const dadoBuffer = arquivo_sincronizado.readFileSync('./002.2_json.json')
+const dadoJSON = dadoBuffer.toString()
+const usuario = JSON.parse(dadoJSON)
 
+usuario.nome = 'Tom'
+usuario.idade = 19
 
+const usuarioJSON = JSON.stringify(usuario)
+arquivo_sincronizado.writeFileSync('./002.2_json.json', usuarioJSON)
 
-
-
+// executar node 002.1.js
 
