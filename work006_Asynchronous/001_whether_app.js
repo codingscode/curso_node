163caf0bc09444c86745c0e2fcc42b947092ff11
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-14 23:03:15 -0300

    part 52

--- work006_Asynchronous/001_whether_app.js
+++ work006_Asynchronous/001_whether_app.js
@@ -2,8 +2,11 @@ console.log('Iniciando')
 
 setTimeout(() => {
     console.log('1 segundo cronometrado')
- }, 1000)
+}, 1000)
 
+setTimeout(() => {
+    console.log('0 segundo cronometrado')
+}, 0)
 
 
 console.log('Parando')
