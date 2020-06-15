a56c34c62aba9d33f7d7188bdf94f9e59ed3969e
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-14 22:28:37 -0300

    parte 49

---
+++ work005_debug/002.2_app.js
@@ -0,0 +1,66 @@
+const giz = require('chalk')
+const yargs = require('yargs')
+const notas = require('./002_notas') 
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'adicionar2',
+    describe: 'adicionar uma nova nota',
+    builder: {
+        titulo: {
+            describe: 'Título Nota',
+            demandOption: true, // false
+            type: 'string' // comentar
+        },
+        corpo: {
+            describe: 'corpo Nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notas.adicionarNota(argv.titulo, argv.corpo)
+    }
+})
+
+yargs.command({
+    command: 'remover2',
+    describe: 'remover uma nota',
+    builder: {
+        titulo: {
+            describe: 'Titulo Nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notas.removerNota(argv.titulo)
+    }
+})
+
+yargs.command({
+    command: 'listar1',
+    describe: 'Liste suas notas',
+    handler() {
+        notas.listarNotas()
+    }
+})
+
+yargs.command({
+    command: 'ler1',
+    describe: 'Leia uma nota',
+    builder: {
+        titulo: {
+           describe: 'Titulo Nota',
+           demandOption: true,
+           type: 'string'
+        }
+    },
+    handler(argv) {
+        notas.lerNota(argv.titulo)
+    }
+})
+
+
+yargs.parse()
---
+++ work005_debug/002_notas.js
@@ -0,0 +1,105 @@
+const arquivo_sincronizado = require('fs')
+const chalk = require('chalk')
+
+ 
+const adicionarNota = (titulo, corpo) => {
+    const notas = carregarNotas()
+    
+    const notaDuplicada = notas.find((cada) => cada.titulo === titulo)
+
+    debugger
+
+    if (!notaDuplicada) {
+        notas.push({
+            titulo: titulo,
+            corpo: corpo
+        })
+        salvarNotas(notas)
+        console.log(chalk.green.inverse('Nova nota adicionada.'))    
+    }
+    else {
+        console.log(chalk.red.inverse('Nova nota ocupada'))
+    }
+
+    //console.log(notas)   
+}
+
+const removerNota = (titulo) => {
+    const notas = carregarNotas()
+    const notasparamanter = notas.filter((nota) => nota.titulo !== titulo)
+
+    if (notas.length > notasparamanter.length) {
+        console.log(chalk.green.inverse('Nota removida'))
+        salvarNotas(notasparamanter)
+    }
+    else {
+        console.log(chalk.red.inverse('Nenhuma nota encontrada'))
+    }
+}
+
+const listarNotas = () => {
+    const notas = carregarNotas()
+
+    console.log(chalk.inverse('Suas notas'))
+
+    notas.forEach((cada) => {
+        console.log(cada.titulo)
+    });
+}
+
+const lerNota = (titulo) => {
+    const notas = carregarNotas()
+    const nota = notas.find((cada) => cada.titulo === titulo)
+
+    if (nota) {
+        console.log(chalk.inverse(nota.titulo))
+        console.log(nota.corpo)
+    }
+    else {
+        console.log(chalk.red.inverse('Nota não encontrada!'))
+    }
+}
+
+const salvarNotas = (notas) => {
+    const dadoJSON = JSON.stringify(notas)
+    arquivo_sincronizado.writeFileSync('notas_criadas1.json', dadoJSON)
+}
+
+const carregarNotas = () => {
+    try {
+        const dadoBuffer = arquivo_sincronizado.readFileSync('notas_criadas1.json') //
+        const dadoJSON = dadoBuffer.toString()
+        return JSON.parse(dadoJSON)
+    }
+    catch(errro) {
+        return []
+    }
+    
+}
+
+module.exports = {
+    adicionarNota: adicionarNota,
+    removerNota: removerNota,
+    listarNotas: listarNotas,
+    lerNota: lerNota
+}
+
+// executar node 001.2_app.js
+// executar node 001.2_app.js adicionar2 --titulo="t" --corpo="c"
+//  node 001.2_app.js adicionar2 --titulo="t" --corpo="c"
+
+//  node 001.2_app.js adicionar2 --titulo="Lista" --corpo="camisa, calças"
+//  node 001.2_app.js adicionar2 --titulo="Lista 2" --corpo="alicate, extensão, pendrive, hd externo"
+
+//  node 001.2_app.js remover2 --titulo="algum titulo"
+//  node 001.2_app.js remover2 --titulo="Lista 3"
+//  node 001.2_app.js remover2 --titulo="Lista 2"
+
+// node 001.2_app.js listar1
+
+// node 001.2_app.js ler1 --titulo="Lista"
+
+//  node 001.2_app.js adicionar2 --titulo="Cursos" --corpo="node.js"
+//  node --inspect-brk 001.2_app.js adicionar2 --titulo="Cursos" --corpo="node.js"
+
+
