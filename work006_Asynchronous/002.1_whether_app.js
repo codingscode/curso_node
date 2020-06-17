250625ec12d3156e04dbee1fe8d355ad91356c17
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-16 22:05:55 -0300

    parte 54

--- work006_Asynchronous/002.1_whether_app.js
+++ work006_Asynchronous/002.1_whether_app.js
@@ -1,9 +1,14 @@
 
+const request = require('request')
+
+
+const url = 'http://api.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233'
+
+request({url: url}, (erro, resposta) => {
+    console.log(resposta)
+})
 
 
 
 
-
-
-
-
+// node 002.1_whether_app.js
