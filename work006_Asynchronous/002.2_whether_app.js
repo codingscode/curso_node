5b187c33078822a27538f2028e286a93813b527e
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-19 00:09:22 -0300

    parte 60

--- work006_Asynchronous/002.2_whether_app.js
+++ work006_Asynchronous/002.2_whether_app.js
@@ -2,8 +2,8 @@
 const request = require('request')
 
 
-const url = 'http://api.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233'
-
+const url = 'http://api.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233?units=si'
+                                                                                                                    // ?key=value&otherKey=otherValue
 request({url: url, json: true}, (erro, resposta) => {
     //console.log(resposta.body.current)
     console.log(`Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
