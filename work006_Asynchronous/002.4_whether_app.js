c17607b2fbce13defddd577649b8d255eebcffdf
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-20 23:52:21 -0300

    parte 66

--- work006_Asynchronous/002.4_whether_app.js
+++ work006_Asynchronous/002.4_whether_app.js
@@ -5,8 +5,15 @@ const request = require('request')
 const url = 'http://xapi.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233'
                                                                                                                     // ?key=value&otherKey=otherValue
 request({url: url, json: true}, (erro, resposta) => {
-    console.log('***', erro)
-    //console.log(`Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
+    if (erro) {
+       //console.log('***', erro)
+       console.log('nao é possivel conectar ao serviço weather')
+    }
+    else {
+        console.log(`Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
+    }
+    
+    //
 })
 
 /* const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
