85cc727c7ebbde5684650ec4818f15e7e1268171
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-21 00:21:36 -0300

    parte 69

--- work006_Asynchronous/002.4_whether_app.js
+++ work006_Asynchronous/002.4_whether_app.js
@@ -2,7 +2,7 @@
 const request = require('request')
 
 
-const url = 'http://api.weatherstack.com/current?access_key=b8f519c2ac25ebbef2903c4bde501052&query='
+/* const url = 'http://api.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233'
                                                                                                                     // ?key=value&otherKey=otherValue
 request({url: url, json: true}, (erro, resposta) => {
     if (erro) {
@@ -17,15 +17,21 @@ request({url: url, json: true}, (erro, resposta) => {
     }
     
     //
-})
+}) */
 
-/* const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
+const geocodeURL = 'https://xapi.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
 
 request({url: geocodeURL, json: true}, (erro, resposta) => {
-    const latitude = resposta.body.features[0].center[1]
-    const longitude = resposta.body.features[0].center[0]
-    console.log(`latitude: ${latitude}, longitude: ${longitude}`)
-}) */
+    if (erro) {
+        console.log('nao foi possivel conectar ao servico de localização')
+    }
+    else {
+        const latitude = resposta.body.features[0].center[1]
+        const longitude = resposta.body.features[0].center[0]
+        console.log(`latitude: ${latitude}, longitude: ${longitude}`)
+    }
+    
+})
 
 
 // node 002.4_whether_app.js
