5df9d729d71343810c5f24d4368c447098d1c283
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-20 23:35:32 -0300

    parte 64

--- work006_Asynchronous/002.4_whether_app.js
+++ work006_Asynchronous/002.4_whether_app.js
@@ -2,11 +2,11 @@
 const request = require('request')
 
 
-const url = 'http://api.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233'
+const url = 'http://xapi.weatherstack.com/current?access_key=senha&query=37.8267,-122.4233'
                                                                                                                     // ?key=value&otherKey=otherValue
 request({url: url, json: true}, (erro, resposta) => {
-    //console.log(resposta.body.current)
-    console.log(`Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
+    console.log(erro)
+    //console.log(`Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
 })
 
 /* const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
