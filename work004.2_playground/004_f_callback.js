95c616849a47cecf4d0a8ed8c2285986d43bc88e
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-21 23:24:24 -0300

    parte 74

--- work004.2_playground/004_f_callback.js
+++ work004.2_playground/004_f_callback.js
@@ -14,13 +14,16 @@ console.log(nomescurtos)
 const geocode = (endereco, callback) => {
     setTimeout(() => {
         const dado = {latitude: 0, longitude: 0}
-        return dado
+        callback(dado)
     }, 2000)
     
 }
 
-const dado = geocode('Philadelphia')
-console.log(dado)
+geocode('Philadelphia', (param) => {
+    console.log(param)
+})
+
+
 
 
 
