f82de0e17b3da521ef8dca2eb8c63460c99130f1
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-24 01:13:59 -0300

    parte 79

--- work006_Asynchronous/002.5_weather_app.js
+++ work006_Asynchronous/002.5_weather_app.js
@@ -37,22 +37,28 @@ request({url: geocodeURL, json: true}, (erro, resposta) => {
 })
  */
 
-const geocode = (endereco, callback) => {
+const geocode = (endereco, funcao) => {
     const link = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(endereco)}.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1`
     
     request({url: link, json: true}, (erro, resposta) => {
         if (erro) {
-           callback('nao foi possivel conectar ao servico de localizacao!', undefined)
+           funcao('nao foi possivel conectar ao servico de localizacao!', undefined)
         }
         else if (resposta.body.features.length === 0) { 
-           callback('nao foi possivel encontrar localizacao. Tente outra busca.', undefined)
+           funcao('nao foi possivel encontrar localizacao. Tente outra busca.', undefined)
         }
-
+        else {
+            funcao(undefined, {
+                latitude: resposta.body.features[0].center[1],
+                longitude: resposta.body.features[0].center[0],
+                localizacao: resposta.body.features[0].place_name
+            })
+        }
     })
 }
 
 
-geocode('12what', (erro, dado) => {
+geocode('Philadelphia', (erro, dado) => {
      console.log('Erro', erro)    
      console.log('Dado', dado)
 })
