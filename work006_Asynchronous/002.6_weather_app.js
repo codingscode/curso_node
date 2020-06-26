0fc8bb93408088c9d4f4da00ec21b71bbe3daeb1
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-25 23:23:15 -0300

    parte 86

--- work006_Asynchronous/002.6_weather_app.js
+++ work006_Asynchronous/002.6_weather_app.js
@@ -3,48 +3,14 @@ const request = require('request')
 
 const previsao = require('./002.6exp_weather_app')
 
-/* const url = 'http://api.weatherstack.com/current?access_key=b8f519c2ac25ebbef2903c4bde501052&query=37.8267,-122.4233'
-                                                                                                                    // ?key=value&otherKey=otherValue
-request({url: url, json: true}, (erro, resposta) => {
-    if (erro) {
-       //console.log('***', erro)
-        console.log('nao é possivel conectar ao serviço weather')
-    }
-    else if (resposta.body.error) {
-        console.log('não é possível encontrar a localização')
-    }
-    else {
-        console.log(`Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
-    }
-    
-    //
-}) */
 
-/* const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
-
-request({url: geocodeURL, json: true}, (erro, resposta) => {
-    if (erro) {
-        console.log('nao foi possivel conectar ao servico de localização')
-    }
-    else if (resposta.body.features.length === 0) {
-        console.log('não é possivel encontrar localização. Tente outra pesquisa.')
-    }
-    else {
-        const latitude = resposta.body.features[0].center[1]
-        const longitude = resposta.body.features[0].center[0]
-        console.log(`latitude: ${latitude}, longitude: ${longitude}`)
-    }
-    
-})
- */
-
-/* geocode('Philadelphia New York', (erro, dado) => {  // tentar Boston
+geocode('Philadelphia', (erro, dado) => {  
      console.log('Erro', erro)    
      console.log('Dado', dado)
-}) */
+})
 
 
-previsao('', '', (erro, dado) => {   // ???
+previsao(75.7088, 44.1545, (erro, dado) => {   // ???
     console.log('Erro:', erro)
     console.log('Dado:', dado)
 })
