355bf72a08166dd2ec781c18c75a4bdbd210fed7
Heronnes Pereira <heronnestg@hotmail.com>
2020-06-28 21:46:35 -0300

    parte 92

--- work006_Asynchronous/002.7_weather_app.js
+++ work006_Asynchronous/002.7_weather_app.js
@@ -3,15 +3,17 @@ const request = require('request')
 const geocode = require('./002.5_weather_app/geocode')
 const previsao = require('./002.6exp_weather_app')
 
+const endereco = process.argv[2]
 
-geocode('Boston', (erro, dado) => {  
+
+
+console.log(process.argv)
+
+geocode(endereco, (erro, dado) => {  
      if (erro) {
          return console.log(erro)
      }
      
-     /* console.log('Erro', erro)    
-     console.log('Dado', dado) */
-   
      previsao(dado.latitude, dado.longitude, (erro, dadoPrevisao) => {  
         if (erro) {
             return console.log(erro)
@@ -19,11 +21,15 @@ geocode('Boston', (erro, dado) => {
 
         console.log(dado.localizacao)
         console.log(dadoPrevisao)
-        /* console.log('Erro:', erro)
-        console.log('Dado:', dado) */
+        
      })
           
 })
 
 
-// node 002.7_weather_app.js
+// node 002.7_weather_app.js Boston
+// node 002.7_weather_app.js 'Boston'
+
+// node 002.7_weather_app.js 'New York'
+// node 002.7_weather_app.js Philadelphia
+
