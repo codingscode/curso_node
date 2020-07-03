9045d7cdb16d3b166e8bc5f48f9a9590509a307d
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-02 22:01:10 -0300

    parte 103

--- work004.2_playground/006_raw-https.js
+++ work004.2_playground/006_raw-https.js
@@ -1,6 +1,6 @@
 const https = require('https')
 
-const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
+const url = 'https://xapi.mapbox.com/geocoding/v5/mapbox.places/philadelphia.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1'
 //const url = 'http://api.weatherstack.com/current?access_key=1494aff90574a6d475ddfb8a9a924ae8&query=40,-75'
 
 const requisicao = https.request(url, (resposta) => {
@@ -17,6 +17,10 @@ const requisicao = https.request(url, (resposta) => {
         const corpo = JSON.parse(dado)
         console.log(corpo)
     })
+})
+
+requisicao.on('error', (erro) => {
+    console.log('Um erro:', erro)
 })
 
 requisicao.end()
