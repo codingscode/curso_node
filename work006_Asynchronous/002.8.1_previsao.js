868de6726eca0db1285a564562510d5e8df3e5fc
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-02 21:19:19 -0300

    parte 98

--- work006_Asynchronous/002.8.1_previsao.js
+++ work006_Asynchronous/002.8.1_previsao.js
@@ -4,15 +4,15 @@ const prever = (latitude, longitude, funcao) => {
     const url = `http://api.weatherstack.com/current?access_key=1494aff90574a6d475ddfb8a9a924ae8&query=${latitude},${longitude}`
     
     console.log(url)
-    request({url: url, json: true}, (erro, resposta) => {
+    request({url, json: true}, (erro, {body}) => {
        if (erro) {
            funcao('Não é possivel conectar ao servico weather', undefined)
        }
-       else if (resposta.body.error) {
+       else if (body.error) {
            funcao('Não é possíve encontrar localização', undefined)
        }
        else {
-           funcao(undefined, `Isto é atualmente: ${resposta.body.current.temperature} graus. Há uma probabilidade de precipitação de ${resposta.body.current.precip} %`)
+           funcao(undefined, `Isto é atualmente: ${body.current.temperature} graus. Há uma probabilidade de precipitação de ${body.current.precip} %`)
        }
     })
 }
--- work006_Asynchronous/002.8.2_geocode.js
+++ work006_Asynchronous/002.8.2_geocode.js
@@ -3,18 +3,18 @@ const request = require('request')
 const geocode = (endereco, funcao) => {
     const link = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(endereco)}.json?access_token=pk.eyJ1IjoiaGVyb25uZXMiLCJhIjoiY2tib2VjbHA0MHFlZjJ5bzZrcHJlN2g3ZSJ9.nFfsqkrzJ9HmMTyB-QMucw&limit=1`
     
-    request({url: link, json: true}, (erro, resposta) => {
+    request({url: link, json: true}, (erro, {body}) => {
         if (erro) {
            funcao('nao foi possivel conectar ao servico de localizacao!', undefined)
         }
-        else if (resposta.body.features.length === 0) { 
+        else if (body.features.length === 0) { 
            funcao('nao foi possivel encontrar localizacao. Tente outra busca.', undefined)
         }
         else {
             funcao(undefined, {
-                latitude: resposta.body.features[0].center[1],
-                longitude: resposta.body.features[0].center[0],
-                localizacao: resposta.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                localizacao: body.features[0].place_name
             })
         }
     })
