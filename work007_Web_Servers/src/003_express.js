8f7a3d94b831d9708c83752f2fdd6303a44ea4e6
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-05 23:25:07 -0300

    parte 111

---
+++ work007_Web_Servers/src/003_express.js
@@ -0,0 +1,37 @@
+const express = require('express')
+
+const aplicacao = express()
+
+aplicacao.get('', (req, res) => {
+    res.send('<h1>Clima</h1>')
+})
+
+aplicacao.get('/ajuda', (req, res) => {
+    res.send([
+        {nome: 'Kyle', idade: 19},
+        {nome: 'sam'}
+    ])
+})
+
+aplicacao.get('/sobre', (req, res) => {
+    res.send('<h1>Sobre</h1>')
+})
+
+aplicacao.get('/clima', (req, res) => {
+    res.send({
+        previsao: 'Sol', localizacao: 'Fortaleza'
+    })
+})
+
+
+aplicacao.listen(3000, () => {
+    console.log('Servidor está funcionando na porta 3000')
+})
+
+
+
+
+// nodemon src/002_express.js
+// localhost:3000
+// localhost:3000/ajuda será um json
+// 
