d6f35676052ad83fc8f4f9cd60eb4df62de3ea2d
Heronnes Pereira <heronnestg@hotmail.com>
2020-07-07 22:12:20 -0300

    parte 118

---
+++ work007_Web_Servers/src/004_express.js
@@ -0,0 +1,31 @@
+const caminho = require('path')
+
+const express = require('express')
+
+
+const aplicacao = express()
+
+const caminhoDiretorioPublic = caminho.join(__dirname, '../public/')
+
+aplicacao.use(express.static(caminhoDiretorioPublic))
+
+
+aplicacao.get('/clima', (req, res) => {
+    res.send({
+        previsao: 'Sol', localizacao: 'Fortaleza'
+    })
+})
+
+
+aplicacao.listen(3000, () => {
+    console.log('Servidor está funcionando na porta 3000')
+})
+
+
+
+
+// nodemon src/004_express.js
+// localhost:3000
+// localhost:3000/ajuda.html
+// localhost:3000/sobre.html
+// 
