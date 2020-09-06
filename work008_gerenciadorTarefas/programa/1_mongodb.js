d63bbc8846023671804a2e40fe0f10f9ac921b57
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-06 00:00:53 -0300

    parte 187

--- work008_gerenciadorTarefas/programa/1_mongodb.js
+++ work008_gerenciadorTarefas/programa/1_mongodb.js
@@ -11,11 +11,11 @@ const { MongoClient, ObjectID } = require('mongodb')
 const conexaoURL = 'mongodb://127.0.0.1:27017'
 const nomeBancoDados = 'gerenciador-tarefa'
 
-const id = new ObjectID()
+/* const id = new ObjectID()
 console.log(id.id)
 console.log(id.id.length)
 console.log(id.toHexString())
-console.log(id.toHexString().length)
+console.log(id.toHexString().length) */
 
 
 MongoClient.connect(conexaoURL, {useNewUrlParser: true}, (erro, cliente) => {
@@ -26,39 +26,13 @@ MongoClient.connect(conexaoURL, {useNewUrlParser: true}, (erro, cliente) => {
     console.log('Conectado corretamente!')
     const bancodados = cliente.db(nomeBancoDados)
 
-    /* bancodados.collection('usuarios').insertOne({
-        nome: 'Michael', idade: 16
-    }, (erro, resultado) => {
-        if (erro) {
-           return console.log('Não foi possível inserir usuário')
-        }
-     
-        console.log(resultado.ops)
-     }) */
-
-     /* bancodados.collection('usuarios').insertMany([
-            { nome: 'Chan', idade: 21},
-            { nome: 'Tom', idade: 30}
-        ], (erro, resultado) => {
-             if (erro) {
-                return console.log('Não foi possível inserir usuário')
-             }
-          
-             console.log(resultado.ops)
-          }
-     ) */
-
-     /* bancodados.collection('tarefas').insertMany([
-         {descricao: 'Codar em javascript', completo: true},
-         {descricao: 'Ir ao mercado', completo: false},
-         {descricao: 'Caminhada', completo: false}
-     ], (erro, resultado) => {
-        if (erro) {
-           return console.log('Não foi possível inserir usuário')
-        }
-     
-        console.log(resultado.ops)
-     }) */
+    bancodados.collection('usuarios').findOne({nome: 'Chan'}, (erro, usuario) => {
+         if (erro) {
+            return console.log('Não foi possível encontrar')
+         }
+   
+         console.log(usuario)
+    })
 
 
     
