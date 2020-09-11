90157db0948b493952e97a1df7865e8edb7fc1f0
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-11 10:26:34 -0300

    parte 197

--- work004.2_playground/008_promises.js
+++ work004.2_playground/008_promises.js
@@ -1,5 +1,15 @@
 // Promises
 
+const trabalharPromise = new Promise((resolver, rejeitar) => {
+    setTimeout(() => {
+        resolver([4, 5, 6])
+    }, 2000)
+})
+
+trabalharPromise.then((resultado) => {
+    console.log('Sucesso', resultado)
+})
 
 
 
+// node 008_promises.js
