2ae0f1488784c5ad6cd85111c1757644901d1300
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-19 23:52:40 -0300

    parte 211

--- work008_gerenciadorTarefas/programa/src/db/mongoose.js
+++ work008_gerenciadorTarefas/programa/src/db/mongoose.js
@@ -6,30 +6,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/gerenciador-tarefa-api', {
 })
 
 const Usuario = mongoose.model('Usuario', {
-    nome: { type: String },
+    nome: { type: String, required: true },
     idade: { type: Number }
 })
 
-const Tarefa = mongoose.model('Tarefa', {
-    descricao: { type: String },
-    completo: { type: Boolean }
-})
-
-const tarefa = new Tarefa({
-    descricao: 'Aprender a biblioteca Mongoose', completo: false
-})
-
-tarefa.save().then((tarefa) => {
-    console.log('tarefa:', tarefa)
-}).catch((erro) => {
-    console.log('erro:', erro)
-})
-
-/*
-
 const eu = new Usuario({
-    nome: 'Bob',
-    idade: 20
+    
 })
 
 eu.save().then((res) => {
@@ -37,7 +19,25 @@ eu.save().then((res) => {
  }).catch((erro) => {
     console.log('Erro:', erro)
  })
- */
+
+const Tarefa = mongoose.model('Tarefa', {
+    descricao: { type: String },
+    completo: { type: Boolean }
+})
+
+/* const tarefa = new Tarefa({
+    descricao: 'Aprender a biblioteca Mongoose', completo: false
+})
+
+tarefa.save().then((tarefa) => {
+    console.log('tarefa:', tarefa)
+}).catch((erro) => {
+    console.log('erro:', erro)
+}) */
+
+
+
+
 
 // node src/db/mongoose.js
 
