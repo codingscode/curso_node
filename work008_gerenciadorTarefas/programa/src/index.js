94de0f677049d4cc62a38766b7fee51f4c4c049b
codinscode <heronnestg@hotmail.com>
2021-07-28 12:26:06 -0300

    rev-3

--- work008_gerenciadorTarefas/programa/src/index.js
+++ work008_gerenciadorTarefas/programa/src/index.js
@@ -1,5 +1,14 @@
 
 
+const express = require('express')
+
+const app = express()
+const porta = process.env.PORT || 3000
+
+app.listen(porta, () => {
+   console.log(`Servidor esta apto na porta {porta}`)
+})
+
 
 
 
